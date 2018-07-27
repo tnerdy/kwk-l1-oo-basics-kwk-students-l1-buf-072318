8c8a082c9c9413528da24e7a2248c22d60a08e71
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-27 18:32:59 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,11 +4,17 @@
 class Book 
   
   attr_reader :title
-  attr_accessor :author
+  attr_accessor :author, :page_count, :genre
   
   def initialize(title) 
     @title = title 
   end
 
-  
+def turn_page
+  puts "Flipping the page...wow, you read fast"
 end
+ 
+ 
+end 
+  
+
