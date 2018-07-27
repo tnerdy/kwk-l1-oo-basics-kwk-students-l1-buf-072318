e138a9aaaebfcfae529093bc5a40aa8727f74d48
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-27 18:27:18 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,11 @@
 
 class Book 
   
-attr_reader :title
-attr_accessor :author
-def initialize(title) 
-  @title = title 
-end
+  attr_reader :title
+  attr_accessor :author
+  def initialize(title) 
+    @title = title 
+  end
 
   
 end
