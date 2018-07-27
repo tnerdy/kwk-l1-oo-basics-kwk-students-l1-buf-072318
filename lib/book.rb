2da8c04016f0eb10c78744868ac4af34cf5a8544
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-27 19:22:16 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
 # book.rb
 # Add you Book class here
+
+class Book 
+  
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title) 
+    @title = title 
+  end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+ 
+end 
+
+the_great_gatsby = Book.new("The Great Gatsby")  
+the_great_gatsby.author = "F Scott Fitzgerald"
+
+
+the_great_gatsby.genre = "The Great American Novel"
+
+
+brave_new_world = Book.new("Brave New World")
+
+puts the_great_gatsby.title 
+puts brave_new_world.title
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+attr_accessor :brand, :color, :size, :material, :condition
+
+ def initialize(brand)
+  @brand = brand 
+ end 
+ 
+def cobble 
+  @condition = "new"
+  puts "Your shoe is as good as new!"
+  
+ end
+
+end
+adidas = Shoe.new("adidas")
+adidas.color = "red"
+adidas.size =  "9.5"
+adidas.material = "suede"
+adidas.condition = "tattered"
+
+
